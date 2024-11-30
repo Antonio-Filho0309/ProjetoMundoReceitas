ddee14bf61c78ed63255328a3186a3c4e85ba197
Toin <antoniodollmichael@gmail.com>
2024-11-29 23:02:50 -0300

    commit6

--- ProjetoMundoReceitas/obj/Debug/net8.0/ProjetoMundoReceitas.AssemblyInfo.cs
+++ ProjetoMundoReceitas/obj/Debug/net8.0/ProjetoMundoReceitas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMundoReceitas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32148a41b4d04ccb6bfaaf4e7c56c64b159c9ddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e07401c553feefff919baaf1ab57bad33b86b963")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMundoReceitas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMundoReceitas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
